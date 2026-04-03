446a124d299b19eb56ed55b854f9e93f83006dad
ericksantosl <156433773+ericksantosl@users.noreply.github.com>
2026-04-03 19:15:49 -0300

    inicio

---
+++ routes/usuario.js
@@ -0,0 +1,95 @@
+const express = require('express')
+const router = express.Router()
+const mongoose = require('mongoose')
+require('../models/Usuario')
+const Usuario = mongoose.model('usuarios')
+const bcrypt = require('bcryptjs')
+const passport = require('passport')
+
+router.get('/registro', (req, res) => {
+    res.render('usuarios/registro')
+})
+
+router.post('/registro', (req, res) => {
+    var erros = []
+
+    if(!req.body.nome || req.body.nome == null) {
+        erros.push({texto: "Nome inválido"})
+    }
+    
+    if(!req.body.email || req.body.email == null) {
+        erros.push({texto: "Email inválido"})
+    }
+
+    if(!req.body.senha || req.body.senha == null) {
+        erros.push({texto: "Senha inválida"})
+    }
+
+    if(!req.body.senha || req.body.senha.length < 4) {
+        erros.push({texto: "Senha muito curta"})
+    }
+
+    if(req.body.senha !== req.body.senha2) {
+        erros.push({texto: "As senhas não coincidem"})
+    }
+
+    if(erros.length > 0) {
+        res.render('usuarios/registro', {erros: erros})
+    } else {
+        Usuario.findOne({email: req.body.email}).lean().then((usuario) => {
+            if(usuario) {
+                req.flash('error_msg', "Erro ao cadastrar usuário")
+                res.redirect('/usuarios/registro')
+            } else {
+                const novoUsuario = new Usuario ({
+                    nome: req.body.nome,
+                    email: req.body.email,
+                    senha: req.body.senha
+                })
+                bcrypt.genSalt(10, (erro, salt) => {
+                    bcrypt.hash(novoUsuario.senha, salt, (erro, hash) => {
+                        if(erro) {
+                           req.flash('error_msg', "Houve um erro ao salvar o usuário")
+                           res.redirect('/')
+                        }
+                        novoUsuario.senha = hash
+                        novoUsuario.save().then(() => {
+                            req.flash('success_msg', "Usuário cadastrado com sucesso")
+                            res.redirect('/')
+                        }).catch((err) => {
+                            req.flash('error_msg', "Houve um erro ao cadastrar o usuário, tente novamente")
+                            res.redirect('/usuarios/registro')
+                        })
+                    })
+                })
+            }
+        }).catch((err) => {
+            req.flash('error_msg', "Houvbe um erro interno")
+            res.redirect('/')
+        })
+    } 
+})
+
+router.get('/login', (req, res) => {
+    res.render('usuarios/login')
+})
+
+router.post(('/login'), (req, res, next) => {
+    passport.authenticate('local', {
+        successRedirect: '/',
+        failureRedirect: '/usuarios/login',
+        failureFlash: true
+    })(req, res, next)
+})
+
+router.get('/logout', (req, res, next) => {
+    req.logout((err) => {
+        if (err) {
+            return next(err)
+        }
+        req.flash('success_msg', "Deslogado com sucesso")
+        res.redirect('/')
+    })
+})
+
+module.exports = router
